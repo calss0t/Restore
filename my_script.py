f3ef1e1c06dc0c620cc0e5e9b92c94df0e3ae678
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-12-10 15:27:43 +0900

    Finished TicTacToe and strarting styling

--- my_script.py
+++ my_script.py
@@ -4,6 +4,38 @@ import random
 from js import document, console
 from pyodide.ffi import create_proxy
 
+def rock_paper_scissors(*args, **kwargs):
+        
+            userInput = Element('rock-paper-scissors-input').element.value
+            # user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+            computer = random.choice(['r', 'p', 's'])
+
+            console.log(f'text: {userInput}')
+        
+            if userInput == computer:
+                pyscript.write("result", 'It\'s a tie')
+        
+            # r > s, s > p, p > r
+            if is_win(userInput, computer):
+                pyscript.write("result",'You won!')
+            
+            else:
+                pyscript.write("result",'You lost!')
+        
+def is_win(player, opponent):
+            # return true if player wins
+            # r > s, s > p, p > r
+            if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+                or (player == 'p' and opponent == 'r'):
+                return True
+        
+                
+function_proxy = create_proxy(rock_paper_scissors)
+
+document.getElementById("button").addEventListener("click", function_proxy)   
+
+
+
 
 
 
